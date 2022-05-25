56dd588f188f92614fe7145b107a688bcfbaebb9
tronglee3107 <52869889+tronglee3107@users.noreply.github.com>
2022-05-25 22:26:09 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# PusherPoll
+
+> Real-Time polling application using Pusher API, Node.js, Express, MongoDB  and CanvasJS
+
+## Quick Start
+
+Edit the config/keys.js file and add your MongoDB URI and Pusher key info
+
+```bash
+# Install dependencies
+npm i
+
+# Run dev server localhost:3000
+npm run dev
+
+# Start
+npm start
+```
---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb://trongdeptrai:trongratdeptrai@votingdatabase-shard-00-00.g9l45.mongodb.net:27017,votingdatabase-shard-00-01.g9l45.mongodb.net:27017,votingdatabase-shard-00-02.g9l45.mongodb.net:27017/votingdatabase?ssl=true&replicaSet=atlas-146krv-shard-0&authSource=admin&retryWrites=true&w=majority',
+  pusherAppId: '1413867',
+  pusherKey: '6fc0e6bb557644585799',
+  pusherSecret: 'c2b2d8f9c53adc12bd45',
+  pusherCluster: 'ap3',
+  pusherEncrypted: true
+};
